3e9b992b2f9247027ae6037cdbe670834d0f0279
Shan Shanmugaesan <shan.shanmugaesan@sqsltd.co.uk>
2019-02-27 09:47:00 +0000

    Tidied up

--- app.js
+++ app.js
@@ -10,22 +10,21 @@ const path = require('path');
 const routePath = path.join(config.path.app, '/config/routes');
 const routes = require(routePath);
 
+const middlewarePath = path.join(config.path.app, '/middlewares');
+const middlewares = require(middlewarePath);
+
 i18n.configure({
   // setup some locales - other locales default to en silently
   locales: ['en', 'de'], 
   // sets a custom cookie name to parse locale settings from
-  cookie: 'express-with-view', 
+  cookie: 'express-with-view-lang', 
   directory: config.path.app + '/locales'
 });
  
 // cookieParser to expose cookies to req.cookies
 app.use(cookieParser());
-
 // i18n init parses req for language headers, cookies, etc.
 app.use(i18n.init);
-
-const middlewarePath = path.join(config.path.app, '/middlewares');
-const middlewares = require(middlewarePath);
 
 app.set('views', path.join(config.path.app, '/views'));
 app.set('view engine', 'ejs');
